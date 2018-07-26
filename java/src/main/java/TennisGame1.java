c7566d527a29c88faa87e128bfb92532ee61a10d
upsd <up648421@myport.ac.uk>
2018-07-26 15:46:33 +0100

    refactor logic for a score comparison

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class TennisGame1 implements TennisGame {
     
@@ -5,10 +7,16 @@ public class TennisGame1 implements TennisGame {
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
+    private Map tennisScoreLookup = new HashMap<Integer, String>();
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+
+        tennisScoreLookup.put(0, "Love");
+        tennisScoreLookup.put(1, "Fifteen");
+        tennisScoreLookup.put(2, "Thirty");
+        tennisScoreLookup.put(3, "Forty");
     }
 
     public void wonPoint(String playerName) {
@@ -20,7 +28,6 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
         if (areDrawing())
         {
             switch (m_score1)
@@ -50,27 +57,12 @@ public class TennisGame1 implements TennisGame {
         }
         else
         {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            score += tennisScoreLookup.get(m_score1);
+            score += "-";
+            score += tennisScoreLookup.get(m_score2);
+
         }
+
         return score;
     }
 
