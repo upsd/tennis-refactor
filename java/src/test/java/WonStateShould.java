b600ba46cd20affa1022a6c745da311e3243052c
upsd <up648421@myport.ac.uk>
2018-07-27 17:10:52 +0100

    Add tests for WonState.

---
+++ java/src/test/java/WonStateShould.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+import wimbledon.game.players.Player;
+import wimbledon.game.states.WonState;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class WonStateShould {
+
+    @Test
+    public void get_score_when_player_one_has_won() {
+        Player playerOne = mock(Player.class);
+        WonState state = new WonState(playerOne, null);
+
+        when(playerOne.hasWonAgainst(any())).thenReturn(true);
+        when(playerOne.name()).thenReturn("reginald");
+
+        String score = state.getScore();
+
+        verify(playerOne).hasWonAgainst(any());
+        assertThat(score, is("Win for reginald"));
+    }
+
+    @Test
+    public void get_score_when_player_two_has_won() {
+        Player playerOne = mock(Player.class);
+        Player playerTwo = mock(Player.class);
+        WonState state = new WonState(playerOne, playerTwo);
+
+        when(playerOne.hasWonAgainst(any())).thenReturn(false);
+        when(playerTwo.name()).thenReturn("chris");
+
+        String score = state.getScore();
+
+        verify(playerOne).hasWonAgainst(any());
+        assertThat(score, is("Win for chris"));
+
+    }
+}
