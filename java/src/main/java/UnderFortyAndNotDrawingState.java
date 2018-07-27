8397e70fb878715cdbe1154035e8d4b97fada2a8
upsd <up648421@myport.ac.uk>
2018-07-27 11:47:01 +0100

    Change string formatting to improve readability.

--- java/src/main/java/UnderFortyAndNotDrawingState.java
+++ java/src/main/java/UnderFortyAndNotDrawingState.java
@@ -2,14 +2,18 @@ public class UnderFortyAndNotDrawingState implements GameState {
 
     private Player playerOne;
     private Player playerTwo;
+    private final TennisTermTranslator translator;
 
     public UnderFortyAndNotDrawingState(Player playerOne, Player playerTwo) {
         this.playerOne = playerOne;
         this.playerTwo = playerTwo;
+        translator = new TennisTermTranslator();
     }
 
     public String getScore() {
-        TennisTermTranslator translator = new TennisTermTranslator();
-        return translator.fromScore(playerOne.getScore()) + "-" + translator.fromScore(playerTwo.getScore());
+        String playerOneScore = translator.fromScore(playerOne.getScore());
+        String playerTwoScore = translator.fromScore(playerTwo.getScore());
+
+        return String.format("%s-%s", playerOneScore, playerTwoScore);
     }
 }
