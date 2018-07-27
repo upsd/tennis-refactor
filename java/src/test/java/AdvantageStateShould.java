44491f2524ae38f1cb170ccdcb0ce18cd6712d69
upsd <up648421@myport.ac.uk>
2018-07-27 16:41:43 +0100

    Add test for AdvantageState.

---
+++ java/src/test/java/AdvantageStateShould.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Test;
+import wimbledon.game.players.Player;
+import wimbledon.game.states.AdvantageState;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class AdvantageStateShould {
+
+    @Test
+    public void get_score_for_player_one_when_they_have_advantage() {
+        Player playerOne = mock(Player.class);
+        AdvantageState state = new AdvantageState(playerOne, null);
+
+        when(playerOne.hasAdvantageOver(any())).thenReturn(true);
+        when(playerOne.name()).thenReturn("reginald");
+
+        String score = state.getScore();
+
+        verify(playerOne).hasAdvantageOver(any());
+        assertThat(score, is("Advantage reginald"));
+    }
+
+    @Test
+    public void get_score_for_player_two_when_they_have_advantage() {
+        Player playerOne = mock(Player.class);
+        Player playerTwo = mock(Player.class);
+        AdvantageState state = new AdvantageState(playerOne, playerTwo);
+
+        when(playerOne.hasAdvantageOver(any())).thenReturn(false);
+        when(playerTwo.name()).thenReturn("chris");
+
+        String score = state.getScore();
+
+        verify(playerOne).hasAdvantageOver(any());
+        assertThat(score, is("Advantage chris"));
+    }
+}
